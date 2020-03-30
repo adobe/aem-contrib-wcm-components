94c6311cd8bffba8be994d18a3e8825d5c182bc6
Niek Raaijmakers <niek@adobe.com>
2020-03-30 10:05:48 +0200

    Fix aempack

--- react-app/scripts/aempack.js
+++ react-app/scripts/aempack.js
@@ -2,41 +2,26 @@
 process.env.BABEL_ENV = 'development';
 process.env.NODE_ENV = 'development';
 
-const path = require('path');
-const fs = require('fs');
-
-const appDirectory = fs.realpathSync(process.cwd());
-const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
-const clientLibRelativePath = '/apps/contrib/wcm/clientlibs/react-webcomponents';
-const clientLibRoot = resolveApp('../ui.apps/src/content/jcr_root/apps/contrib/wcm/clientlibs/react-webcomponents');
-
-
-
+const paths = require('../config/paths');
 const aemPack = require('aempack');
 
 const developWithSSR = process.env.DEVELOPSSR || false;
-const webpackConfig = require('react-scripts/config/webpack.config.js')('development');
+const webpackConfig = require('./webpackConfig');
 
-webpackConfig.output.path = clientLibRoot;
-webpackConfig.output.publicPath = process.env.PUBLIC_URL;
 const user = process.env.AEMUSER  || 'admin';
 const password = process.env.AEMPW || 'admin';
 
 const aemPort = process.env.AEMPORT || 4502;
 const aemHost = process.env.AEMHOST || 'localhost';
 
-
 const proxyHost = process.env.PROXYHOST || 'localhost';
 const proxyPort = process.env.PROXYPORT || 443;
-
 
 const computeProxyUrl = () => {
 
     const port = proxyPort === 80 ? '' : ':' + aemPort;
     return 'http://' + proxyHost + port;
 };
-
-//const serverWebpackConfig = require('../config/webpack.config.server.dev');
 
 aemPack({
     delays: {
@@ -61,8 +46,8 @@ aemPack({
     aemUser: user,
     aemPassword: password,
 
-    clientLibRelativePath: clientLibRelativePath,
-    clientLibAbsolutePath: clientLibRoot,
+    clientLibRelativePath: paths.clientLibRelativePath,
+    clientLibAbsolutePath: paths.clientLibRoot,
 });
 
 
