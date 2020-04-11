4c832d3c6291e1a6f0979e750d4164f987da7ba0
Niek Raaijmakers <niek@adobe.com>
2020-04-11 13:03:50 +0200

    Add proper test / coverage scripts

--- react/react-core/src/core/common/editorcontext.tsx
+++ react/react-core/src/core/common/editorcontext.tsx
@@ -14,6 +14,14 @@ export const EditorContextConsumer = context.Consumer;
 
 export default context;
 
+export const withEditorContext = <P extends object>(
+    Component: React.ComponentType<P>,
+): React.FC<Omit<P, keyof EditorContext>> => props => (
+    <context.Consumer>
+        {({ wcmmode }) => <Component {...props as P} wcmmode={wcmmode} />}
+    </context.Consumer>
+);
+
 export const setEditorContext = <P extends object>(
     Component: React.ComponentType<P>,
     editorContext: EditorContext
