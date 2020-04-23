2c03f8ad108d56b412a674eb003915ef75fae043
Niek Raaijmakers <niek@adobe.com>
2020-04-23 13:57:37 +0200

    WIP

--- react/react-spacomponents-example-project/src/mapping.js
+++ react/react-spacomponents-example-project/src/mapping.js
@@ -23,30 +23,30 @@ import {
 } from "aem-core-components-contributions-react-core-spa";
 
 
-import { MapTo,withComponentMappingContext, Container} from '@adobe/cq-react-editable-components';
+import { MapTo, Container} from '@adobe/cq-react-editable-components';
 
 import withRoute from './utils/RouteHelper';
 import ContribPage from './components/Page';
 import Demo from './components/Demo';
 
-MapTo('contrib/wcm/components/languagenavigation')(withComponentMappingContext(SpaLanguageNavigationV1));
-MapTo('contrib/wcm/components/navigation')(withComponentMappingContext(SpaNavigationV1));
+MapTo('contrib/wcm/components/languagenavigation')(SpaLanguageNavigationV1);
+MapTo('contrib/wcm/components/navigation')(SpaNavigationV1);
 
 MapTo('contrib/wcm/components/list')(SpaListV2, {isEmpty: ListV2IsEmptyFn});
 MapTo('contrib/wcm/components/separator')(SeparatorV1, {isEmpty: SeparatorV1IsEmptyFn});
 
 MapTo('contrib/wcm/components/download')(DownloadV1, {isEmpty: DownloadV1IsEmptyFn});
 MapTo('contrib/wcm/components/text')(TextV2, {isEmpty: TextV2IsEmptyFn});
-MapTo('contrib/wcm/components/breadcrumb')(withComponentMappingContext(SpaBreadCrumbV2), {isEmpty: BreadCrumbV2IsEmptyFn});
+MapTo('contrib/wcm/components/breadcrumb')(SpaBreadCrumbV2, {isEmpty: BreadCrumbV2IsEmptyFn});
 MapTo('contrib/wcm/components/button')(SpaButtonV1, {isEmpty: ButtonV1IsEmptyFn});
-MapTo('contrib/wcm/components/page/react-spacomponents-page')(withComponentMappingContext(withRoute(ContribPage)), {});
-MapTo('contrib/wcm/components/tabs')(withComponentMappingContext(TabsV2), {isEmpty: TabsV2IsEmptyFn});
-MapTo('contrib/wcm/components/teaser')(withComponentMappingContext(TeaserV1), {isEmpty: TeaserV1IsEmptyFn});
-MapTo('contrib/wcm/components/image')(withComponentMappingContext(ImageV2), {isEmpty: ImageV2IsEmptyFn});
-MapTo('contrib/wcm/components/title')(withComponentMappingContext(TitleV2), {isEmpty: TitleV2IsEmptyFn});
-MapTo('contrib/wcm/components/accordion')(withComponentMappingContext(AccordionV1), {isEmpty: AccordionV1IsEmptyFn});
-MapTo('contrib/wcm/components/carousel')(withComponentMappingContext(CarouselV1), {isEmpty: CarouselV1IsEmptyFn});
-MapTo('contrib/wcm/components/container')(withComponentMappingContext(ContainerV1), {isEmpty: ContainerV1IsEmptyFn});
-MapTo('core-components-examples/components/demo')(withComponentMappingContext(Demo));
-MapTo('core-components-examples/components/demo/component')(withComponentMappingContext(Container));
+MapTo('contrib/wcm/components/page/react-spacomponents-page')(withRoute(ContribPage), {});
+MapTo('contrib/wcm/components/tabs')(TabsV2, {isEmpty: TabsV2IsEmptyFn});
+MapTo('contrib/wcm/components/teaser')(TeaserV1, {isEmpty: TeaserV1IsEmptyFn});
+MapTo('contrib/wcm/components/image')(ImageV2, {isEmpty: ImageV2IsEmptyFn});
+MapTo('contrib/wcm/components/title')(TitleV2, {isEmpty: TitleV2IsEmptyFn});
+MapTo('contrib/wcm/components/accordion')(AccordionV1, {isEmpty: AccordionV1IsEmptyFn});
+MapTo('contrib/wcm/components/carousel')(CarouselV1, {isEmpty: CarouselV1IsEmptyFn});
+MapTo('contrib/wcm/components/container')(ContainerV1, {isEmpty: ContainerV1IsEmptyFn});
+MapTo('core-components-examples/components/demo')(Demo);
+MapTo('core-components-examples/components/demo/component')(Container);
 
--- react/react-spacomponents-example-project/src/renderRoot.jsx
+++ react/react-spacomponents-example-project/src/renderRoot.jsx
@@ -6,14 +6,19 @@ import { ModelManager, Constants } from '@adobe/cq-spa-page-model-manager';
 
 import App from './components/App';
 
-
 const render = (model) => {
-    ReactDOM.render((
-            <BrowserRouter>
-                    <App cqChildren={ model[Constants.CHILDREN_PROP] } cqItems={ model[Constants.ITEMS_PROP] } cqItemsOrder={ model[Constants.ITEMS_ORDER_PROP] }
-                              cqPath={ ModelManager.rootPath } locationPathname={ window.location.pathname }/>
-            </BrowserRouter>),
-        document.getElementById('root'));
+
+    const Root = (
+        <BrowserRouter>
+            <App cqChildren={ model[Constants.CHILDREN_PROP] }
+                 cqItems={ model[Constants.ITEMS_PROP] }
+                 cqItemsOrder={ model[Constants.ITEMS_ORDER_PROP] }
+                 cqPath={ ModelManager.rootPath }
+                 locationPathname={ window.location.pathname }/>
+        </BrowserRouter>
+    );
+
+    ReactDOM.render(Root,document.getElementById('root'));
 };
 
 export default render;
